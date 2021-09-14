c2768c7421de2651ea74c6d960daee10163ecc36
rochagi <grocha0507@gmail.com>
2021-09-14 14:10:31 -0300

    model

--- lib/app/modules/adivice_app/views/pages/advice_app_page.dart
+++ lib/app/modules/adivice_app/views/pages/advice_app_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:proj3/app/modules/adivice_app/data_source/advice_repository.dart';
+import 'package:proj3/app/modules/adivice_app/data_source/advice_repository_impl.dart';
 
-class AdviceAppPage extends StatelessWidget {
+class AdviceAppPage extends StatefulWidget {
   const AdviceAppPage({ Key? key }) : super(key: key);
 
   @override
+  _AdviceAppPageState createState() => _AdviceAppPageState();
+}
+
+class _AdviceAppPageState extends State<AdviceAppPage> {
+  @override
+
+  @override
+  void initState() { 
+    super.initState();
+    AdviceRepository adviceRepository = AdviceRepositoryImpl();
+    adviceRepository.fetchAdvice();
+  }
   Widget build(BuildContext context) {
     return Container(
       
